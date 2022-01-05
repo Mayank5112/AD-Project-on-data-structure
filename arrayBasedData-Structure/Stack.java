f923a93f19fbf3e0c627aca9e2c2e5d2ffe3ae06
Mayank5112 <92751782+Mayank5112@users.noreply.github.com>
2022-01-05 18:01:39 +0530

    Update Stack.java

--- arrayBasedData-Structure/Stack.java
+++ arrayBasedData-Structure/Stack.java
@@ -1,68 +1,74 @@
 import java.util.EmptyStackException;
 
+
+
+public class Stack {
 	
+	private int arr[];
+	private int size;
+	private int index = 0;
 	
-	public class Stack {
+	public Stack(int size) {
+		this.size = size;
+		arr = new int[size];
+	}
+	
+	public void push(int element) {
 		
-		private int arr[];
-		private int size;
-		private int index = 0;
-		
-		public Stack(int size) {
-			this.size = size;
-			arr = new int[size];
+		if (isFull()) {
+			System.out.println("the Stack is full ");
 		}
 		
-		public void push(int element) {
-			
-			if (isFull()) {
-				System.out.println("the Stack is full ");
+		arr[index] = element;
+		index++;
+	}
+	
+	public int pop() {
+		
+		if (isEmpty()) {
+			System.out.println("list is empty");
+		}
+		return arr[--index];
+	}
+	
+	public void  print(){
+		int i = 1;
+		System.out.println("all the elements");
+		while (i<=size)
+		{
+			System.out.println(pop());
+				i++;
 			}
-			
-			arr[index] = element;
-			index++;
-		}
 		
-		public int pop() {
-			
-			if (isEmpty()) {
-				System.out.println("list is empty");
-			}
-			return arr[--index];
+	}
+	public boolean isEmpty() {
+		if (index == 0) {
+			return true;
 		}
-		
-		public boolean isEmpty() {
-			if (index == 0) {
-				return true;
-			}
-			return false;
+		return false;
+	}
+	
+	public boolean isFull() {
+		if (index == size) {
+			return true;
 		}
-		
-		public boolean isFull() {
-			if (index == size) {
-				return true;
-			}
-			return false;
-		}
-		
-		public int size() {
-			return index;
-		}
+		return false;
+	}
+	
+	public int size() {
+		return index;
+	}
 	
 	public static void main(String [] args){
-	
-			Stack stack = new Stack(2);
-			stack.push(2);
-			stack.push(33);
+// 		creat a new stack give it a name --> *s*
+// 		use push method to add elements to the stack --> *s*.push(*element*)
+// 		use stack pop to remove the element --> *s*.pop();
+// 		use print method to display all the elements in the stack --> *s*.print();
+		Stack stack = new Stack(2);
+		stack.push(2);
+		stack.push(33);
+		stack.print();
+
 		
-		System.out.println("size of stack " + stack.size());
-		System.out.print("poping all the elements in stack ");
-		while(!stack.isEmpty()){
-			
-			System.out.print(" " + stack.pop() );
-		}
-		
-		System.out.println("\nsize after all the pop " + stack.size());
-	
-		}
 	}
+}
